885dbe5314ce8e34b138d138f8e861d41d69a241
aubbiali <uba99a@gmail.com>
2022-10-13 10:14:48 +0200

    Added prossimita sensore

---
+++ 11-prossimitaSensore/11-prossimitaSensore.ino
@@ -0,0 +1,28 @@
+#define trigPin 4 //D2
+#define echoPin 5 //D1
+
+float duration, distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) * 0.0343;
+
+  if (distance <= 400 && distance >=2) {
+    Serial.print("Distance = ");
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+
+  delay(150);
+}
